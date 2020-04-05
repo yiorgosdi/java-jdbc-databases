d7828f5cb268cca673a642f987807eac4fef3d09
yiorgosdi <ydimitriades@gmail.com>
2020-04-05 20:00:54 +0300

    Completed module

--- src/main/java/com/pluralsight/order/util/Database.java
+++ src/main/java/com/pluralsight/order/util/Database.java
@@ -5,6 +5,7 @@ import org.h2.tools.RunScript;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
@@ -13,6 +14,10 @@ import java.sql.SQLException;
 public class Database {
     private static Database instance = null;
     private static boolean isInitialized = false;
+
+    String url = "jdbc:h2:mem:orders;DB_CLOSE_DELAY=-1";
+    private String user = "sa";
+    private String password = "";
 
     /**
      * Private constructor
@@ -49,7 +54,7 @@ public class Database {
      * @throws SQLException In case of a database error
      */
     public Connection getConnection() throws SQLException {
-        Connection connection = null;
+        Connection connection = DriverManager.getConnection(url);
 
         if(!isInitialized && connection != null) {
             initializeDatabase(connection);
